19175380900b30fb9225cbef44a52526b6494bb7
TheKingOfFamily <37731091+thekingoffamily@users.noreply.github.com>
2018-06-15 12:00:24 +0300

    Create flag.py

---
+++ flag.py
@@ -0,0 +1,94 @@
+import os, time
+
+a1 = '''
+0__________
+0__________
+0__________
+'''
+a2 = '''
+0__________
+_0_________
+__0________
+'''
+a3 = '''
+_0_________
+__0________
+___0_______
+'''
+a4 = '''
+__0________
+___0_______
+____0______
+'''
+a5 = '''
+___0_______
+____0______
+_____0_____
+'''
+a6 = '''
+____0______
+_____0_____
+______0____
+'''
+a7 = '''
+_____0_____
+______0____
+_______0___
+'''
+a8 = '''
+______0____
+_______0___
+________0__
+'''
+a9 = '''
+_______0___
+________0__
+_________0_
+'''
+a10 = '''
+________0__
+_________0_
+__________0
+'''
+a11 = '''
+__________0
+__________0
+__________0
+'''
+
+
+while 1:
+    os.system('cls')
+    print(a1)
+    time.sleep(0.1)
+    os.system('cls')
+    print(a2)
+    time.sleep(0.1)
+    os.system('cls')
+    print(a3)
+    time.sleep(0.1)
+    os.system('cls')
+    print(a4)
+    time.sleep(0.1)
+    os.system('cls')
+    print(a5)
+    time.sleep(0.1)
+    os.system('cls')
+    print(a6)
+    time.sleep(0.1)
+    os.system('cls')
+    print(a7)
+    time.sleep(0.1)
+    os.system('cls')
+    print(a8)
+    time.sleep(0.1)
+    os.system('cls')
+    print(a9)
+    time.sleep(0.1)
+    os.system('cls')
+    print(a10)
+    time.sleep(0.1)
+    os.system('cls')
+    print(a11)
+    time.sleep(0.1)
+    os.system('cls')
